e991574a0302f86bb751440d891c55465bb4cf2a
hika019 <ikazuti2001@gmail.com>
2021-09-21 13:16:16 +0900

    CreateCourseの教師リストの表示方法の変更

--- app/src/main/java/jp/hika019/kerkar_university/Create_courceActivity.kt
+++ app/src/main/java/jp/hika019/kerkar_university/Create_courceActivity.kt
@@ -2,9 +2,11 @@ package jp.hika019.kerkar_university
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import jp.hika019.kerkar_university.viewmodels.Create_courceVM
 import jp.hika019.kerkar_university.databinding.ActivityCreateCourcetBinding
 
@@ -37,7 +39,23 @@ class Create_courceActivity: AppCompatActivity() {
                 finish()
         })
 
+        viewmodel.createbutton_enable.observe(this, Observer {
+            if(viewmodel.createbutton_enable.value == true){
+                binding.CreateButton.setTextColor(getColor(R.color.logo_color))
+            }
+        })
 
+        viewmodel.course_lecture.observe(this, Observer {
+            binding.lectureLinearLayout.removeAllViews()
+            for(lecturer in viewmodel.course_lecture_list.value!!){
+                val textview = TextView(this)
+                textview.text = lecturer
+                textview.textSize = 12f
+                textview.setPadding(8, 0, 8, 0)
+                binding.lectureLinearLayout.addView(textview)
+            }
+
+        })
 
 
 
